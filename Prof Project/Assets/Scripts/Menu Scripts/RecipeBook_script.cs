8f3fea903d62e64816a49602ea35968eb460c775
McCartanBlair <71771771+McCartanBlair@users.noreply.github.com>
2022-02-15 21:52:31 +0000

    Added own unity project
    Implemented list. clicking on the recipe book will bring up list canvas if not already there

---
+++ Prof Project/Assets/Scripts/Menu Scripts/RecipeBook_script.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class RecipeBook_script : MonoBehaviour
+{
+    public Canvas recipeBook;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tempObject = GameObject.Find("Recipebook Canvas");
+
+        if (tempObject != null)
+        {
+            recipeBook = tempObject.GetComponent<Canvas>();
+            if (recipeBook == null)
+            {
+                Debug.Log("Could not locate canvas object on " + tempObject.name);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+
+            recipeBook.enabled = false;
+        }
+    }
+    
+    void OnMouseDown()
+    {
+        Debug.Log("Hello world this click was detected");
+        // this object was clicked - do something
+
+        if (recipeBook.enabled == false)
+        {
+             recipeBook.enabled = true;
+        }
+    }
+}
---
+++ Prof Project/Assets/Scripts/Menu Scripts/backButton_script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backButton_script : MonoBehaviour
+{
+    public Canvas recipeBook;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tempObject = GameObject.Find("Recipebook Canvas");
+
+        if (tempObject != null)
+        {
+            recipeBook = tempObject.GetComponent<Canvas>();
+            if (recipeBook == null)
+            {
+                Debug.Log("Could not locate canvas object on " + tempObject.name);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Hello world this click was detected");
+        // this object was clicked - do something
+
+        if (recipeBook.enabled == true)
+        {
+            recipeBook.enabled = false;
+        }
+    }
+}
