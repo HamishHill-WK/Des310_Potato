a094a13d789f107c4cf73e31d7a9dc6e78263f22
HamishHill-WK <57226991+HamishHill-WK@users.noreply.github.com>
2022-02-16 15:03:58 +0000

    Menu1
    Extended menu work

--- Des310_Potato/Assets/Scripts/Menu.cs
+++ Des310_Potato/Assets/Scripts/Menu.cs
@@ -1,26 +1,81 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    private GameObject menuPanel;
     private GameObject profilePanel;
+    private GameObject optionsPanel;
+    private GameObject gamePanel;
+
+    enum States { menu = 0, profile, options, closed };
+
+    States current = States.menu;
+
     // Start is called before the first frame update
     void Start()
     {
+        menuPanel = GameObject.Find("MainMenu");
         profilePanel = GameObject.Find("Profile");
+        optionsPanel = GameObject.Find("Options");
+        gamePanel = GameObject.Find("GameView");
+
+        switchState(States.menu);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void enterGame()
     {
-        
+        switchState(States.closed);
     }
 
     public void openProfile()
     {
-        profilePanel.setActive(true);
-        this.set
+        switchState(States.profile);
     }
+
+    public void openMenu()
+    {
+        switchState(States.menu);
+    }
+
+    public void openOptions()
+    {
+        switchState(States.options);
+    }
+
+    void switchState(States state)
+    {
+        current = state;
+        switch (current)
+        {
+            case States.menu:
+                // code block
+                profilePanel.SetActive(false);
+                menuPanel.SetActive(true);
+                optionsPanel.SetActive(false);
+                gamePanel.SetActive(false);
+                break;
+            case States.profile:
+                // code block
+                profilePanel.SetActive(true);
+                menuPanel.SetActive(false);
+                optionsPanel.SetActive(false);
+                gamePanel.SetActive(false);
+                break;
+            case States.options:
+                // code block
+                profilePanel.SetActive(false);
+                menuPanel.SetActive(false);
+                optionsPanel.SetActive(true);
+                gamePanel.SetActive(false);
+                break;            
+            case States.closed:
+                // code block
+                profilePanel.SetActive(false);
+                menuPanel.SetActive(false);
+                optionsPanel.SetActive(false);
+                gamePanel.SetActive(true);
+                break;
+        }
+    }
+    
 }
